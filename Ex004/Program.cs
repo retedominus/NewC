e78abf7141719b55dceba1c8395018d147a0410e
Aleksandr D <bestacc@mail.ru>
2022-08-03 18:44:59 +0300

    Added Special greetings program

---
+++ Ex004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это же Саша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
