c28884e94616783287dc2721791c420d74b8144a
Aleksandr D <bestacc@mail.ru>
2022-08-08 22:25:44 +0300

    Added Code from lecture

---
+++ Ex010/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillArray(int[] collections)
+{
+    int length = collections.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collections[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+while (position < count)
+{
+    Console.WriteLine(col[position]);
+    position++;
+}
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
