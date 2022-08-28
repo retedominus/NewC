37d015cded730a811c4cadf563816fcc93d81265
Aleksandr D <bestacc@mail.ru>
2022-08-28 11:40:50 +0300

    create Fibonacci method

---
+++ Ex1017/Program.cs
@@ -0,0 +1,12 @@
+﻿// Fibonacci
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 10; i++)
+{
+    System.Console.WriteLine(Fibonacci(i));
+}
+System.Console.WriteLine();
+System.Console.WriteLine(Fibonacci(9));
