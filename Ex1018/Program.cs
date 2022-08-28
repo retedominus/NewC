7209305fda95e5fd242d2ddf7f0e8bad23666b74
Aleksandr D <bestacc@mail.ru>
2022-08-28 11:51:35 +0300

    create print on screen method

---
+++ Ex1018/Program.cs
@@ -0,0 +1,15 @@
+﻿// Мктод печати в определенном месте экрана
+void DrawText(string text, int left, int top)
+{
+    Console.SetCursorPosition(left, top);
+    System.Console.WriteLine(text);
+}
+string caption = "Intensive C# Demo Text";
+int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;
+int screenHeightPosition = Console.WindowHeight / 2;
+
+DrawText(
+    text: caption,
+    left: screenWidthPosition,
+    top: screenHeightPosition
+);
