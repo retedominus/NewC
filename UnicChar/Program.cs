975dff6b61404064b7021b020331db95dc117eaf
Aleksandr D <bestacc@mail.ru>
2022-09-02 17:22:33 +0300

    Unique symbol search

---
+++ UnicChar/Program.cs
@@ -0,0 +1,14 @@
+﻿//Поиск уникальных символов
+string s = "cscvsfbvaskncbvskeckafhdayslhicjfsad";
+char[] buf = new char[s.Length];
+int iBuf = 0;
+int i = 0;
+while (i < s.Length)
+{
+    if (Array.IndexOf(buf, s[i]) == -1)
+    {
+        buf[iBuf++] = s[i];
+    }
+    i++;
+}
+Console.WriteLine(String.Join(' ', buf));
