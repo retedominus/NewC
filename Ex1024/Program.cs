3a57dfe9a0889d391065fa051c8d9bf90fca42c7
Aleksandr D <bestacc@mail.ru>
2022-09-02 14:25:14 +0300

    create Folders contents prog

---
+++ Ex1024/Program.cs
@@ -0,0 +1,19 @@
+﻿// Просмотр содержимого папок
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"...";
+CatalogInfo(path);
