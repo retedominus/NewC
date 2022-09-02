01705076deb312638d753c278b783211fe42d077
Aleksandr D <bestacc@mail.ru>
2022-09-02 17:11:16 +0300

    create Dict

---
+++ Ex1027/Program.cs
@@ -0,0 +1,12 @@
+﻿// Частотный словарь
+int[] arr = new int[] { 1, 0, 0, 0, 3, 1, 3, 1, 2, 3, 1, 2, 3, 2, 1, 3, 1, 2 };
+int max = arr.Max();
+int[] dic = new int[max + 1];
+for (int i = 0; i < arr.Length; i++)
+{
+    dic[arr[i]]++;
+}
+for (int i = 0; i < dic.Length; i++)
+{
+    System.Console.WriteLine($"{i} {dic[i]}");
+}
